99b4401371fedb6c633d43e0c85f7d90d6284329
BerenyiPeti <berenyipeti01@gmail.com>
2022-10-18 10:45:11 +0200

    konyv view kezdet

--- Controller/KonyvController.js
+++ Controller/KonyvController.js
@@ -3,14 +3,15 @@ import KonyvModel from "../Model/KonyvModel.js";
 
 class KonyvController {
     constructor() {
-        console.log("KonyvController");
-        new KonyvekView();
+        //console.log("KonyvController");
         const konyvmodel = new KonyvModel();
         konyvmodel.adatBe("../adat.json", this.konyvAdatok);
     }
 
     konyvAdatok(tomb) {
-        console.log(tomb);
+        //console.log(tomb);
+        let szuloelem = $("main")
+        new KonyvekView(tomb, szuloelem)
     }
 }
 
